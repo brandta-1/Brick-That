cb9877d751ba32fc8252b88a5b4c37821160a2f9
Alex Brandt <alexezemail@gmail.com>
2023-05-05 21:35:34 -0400

    added fileIO

--- public/script.js
+++ public/script.js
@@ -1,3 +1,89 @@
+const input = document.querySelector("input")
+const output = document.querySelector("output")
+let imagesArray = []
+
+
+    $('.up-btn').on('click', async (event) => {
+
+        await fetch('/api/lego', {
+            method: 'GET'
+        });
+
+
+    })
+
+input.addEventListener("change", () => {
+    const file = input.files
+    imagesArray.push(file[0])
+    displayImages()
+})
+
+function displayImages() {
+ 
+    output.innerHTML = `<div class="image"> <img src="${URL.createObjectURL(input.files[0])}" alt="image" id="image1"> </div `
+    const brick = new Image();
+    brick.src = 'brick.png';
+
+    //brick is 30x30 px
+
+    let c = document.createElement("canvas");
+    let img1 = new Image();
+
+    img1.src = URL.createObjectURL(input.files[0]);
+
+
+    //dimensions of an image in the brick picture
+    const dim = (img, br) => [img.width / br.width, img.height / br.height];
+
+
+    img1.onload = function () {
+        w = img1.width;
+        h = img1.height;
+
+        c.width = w;
+        c.height = h;
+        ctx = c.getContext("2d");
+        ctx.drawImage(img1, 0, 0);
+
+        let pixelArr = ctx.getImageData(0, 0, w, h).data;
+        let sample_size = 30;
+
+        let backEndArray = [];
+
+
+
+        for (let i = 0; i < h; i += sample_size) {
+            for (let j = 0; j < w; j += sample_size) {
+                let p = (j + (i * w)) * 4;
+
+                // backEndArray.push(pixelArr.slice(p,p+3));
+                backEndArray.push(
+                    pixelArr[p],
+                    pixelArr[p + 1],
+                    pixelArr[p + 2]
+                );
+
+                const currRGBA = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + ",255)"
+                console.log(currRGBA);
+                ctx.fillStyle = currRGBA;
+                ctx.fillRect(j, i, sample_size, sample_size);
+            }
+        }
+
+        console.log(backEndArray);
+
+        let img2 = new Image();
+        img2.src = c.toDataURL("image/jpeg");
+        document.body.appendChild(img2);
+        document.body.appendChild(brick);
+
+    }
+
+}
+
+
+
+/*
 
 let pic = document.getElementById("image1");
 
@@ -38,17 +124,28 @@ img1.onload = function () {
 
     let backEndArray = [];
 
+  
+
     for (let i = 0; i < h; i += sample_size) {
         for (let j = 0; j < w; j += sample_size) {
             let p = (j + (i * w)) * 4;
-            
-           
-            ctx.fillStyle = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + "," + pixelArr[p + 3] + ")";
+
+           // backEndArray.push(pixelArr.slice(p,p+3));
+            backEndArray.push(
+                pixelArr[p],
+                pixelArr[p+1],
+                pixelArr[p+2]
+            );
+
+            const currRGBA ="rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + ",255)"
+            console.log(currRGBA);
+            ctx.fillStyle = currRGBA;
             ctx.fillRect(j, i, sample_size, sample_size);
         }
     }
 
     console.log(backEndArray);
+
     let img2 = new Image();
     img2.src = c.toDataURL("image/jpeg");
     document.body.appendChild(img2);
@@ -60,5 +157,9 @@ img1.onload = function () {
 
 
 document.body.appendChild(brick);
+
+*/
+
+
 
 
