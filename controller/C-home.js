3a864d452bc2c76520bb813ad6e2de4bb2345418
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-05-08 20:33:12 -0400

    fixed some of the navbar on partials, also tweaked routing within that and on controllers

--- controller/C-home.js
+++ controller/C-home.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { User, Lego } = require('../model');
 
 router.get('/', (req, res) => {
@@ -6,5 +7,15 @@ router.get('/', (req, res) => {
     res.render('homepage')
 })
 
+// Router Login
+router.get('/Login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect(
+            '/Homepage'
+        );
+        return;
+    }
+    res.render('login')
+})
 
 module.exports = router;
