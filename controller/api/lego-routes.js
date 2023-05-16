9f40b76961f6d3157ab8fe739aea03719d272535
Alex Brandt <alexezemail@gmail.com>
2023-05-16 18:58:15 -0400

    added imagekit integration via node

--- controller/api/lego-routes.js
+++ controller/api/lego-routes.js
@@ -1,13 +1,42 @@
 const router = require('express').Router();
 const { Model } = require('sequelize');
 const { User, Lego } = require('../../model');
+const ImageKit = require("imagekit");
+require('dotenv').config();
+
+let imagekit;
+
+if (process.env.IMGK_P_KEY) {
+    imagekit = new ImageKit({
+        publicKey: "public_1XYdHv9hugFrHIM6ThiPsM932aM=",
+        privateKey: process.env.IMGK_P_KEY,
+        urlEndpoint: "https://ik.imagekit.io/bzladgycc/"
+    });
+} else {
+    imagekit = new ImageKit({
+        publicKey: "public_1XYdHv9hugFrHIM6ThiPsM932aM=",
+        privateKey: process.env.PRIVATE_KEY,
+        urlEndpoint: "https://ik.imagekit.io/bzladgycc/"
+    });
+}
 
 router.post('/', async (req, res) => {
     try {
-        
-        await Lego.create({
-            ...req.body,
-            user_id: req.session.user_id
+
+
+        imagekit.upload({
+            file: req.body.lego_url,
+            fileName: "test1.jpg"
+        }, async function (error, result) {
+            if (error) console.log(error);
+            else {
+
+                await Lego.create({
+                    stored_url: result.url,
+                    user_id: req.session.user_id
+                });
+
+            }
         });
 
         res.status(200).json("success")
